bd01315dc7c18db1a6cbdf208a4adb73d3eff8b3
EdySnake1 <lestatvelacr@gmail.com>
2021-11-29 19:49:03 -0600

    listo recien subido

---
+++ salario.js
@@ -0,0 +1,57 @@
+const colombia = [];
+colombia.push({
+  name: "juanita",
+  salary: 10000,
+});
+colombia.push({
+  name: "laura",
+  salary: 4000,
+});
+colombia.push({
+  name: "cesar",
+  salary: 6000,
+});
+colombia.push({
+  name: "karla",
+  salary: 6000,
+});
+colombia.push({
+  name: "damian",
+  salary: 10000,
+});
+colombia.push({
+  name: "carlos",
+  salary: 2000,
+});
+colombia.push({
+  name: "arturo",
+  salary: 6000,
+});
+colombia.push({
+  name: "raul",
+  salary: 4000,
+});
+colombia.push({
+  name: "alfonso",
+  salary: 5000,
+});
+colombia.push({
+  name: "federico",
+  salary: 9000,
+});
+colombia.push({
+  name: "jesica",
+  salary: 7000,
+});
+colombia.push({
+  name: "ivon",
+  salary: 5000,
+});
+colombia.push({
+  name: "alejandra",
+  salary: 5000,
+});
+colombia.push({
+  name: "veronica",
+  salary: 10000,
+});
